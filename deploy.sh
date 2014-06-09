aff1848712cdc51eeb10d449dd8c964d0152cbc4
Florian Boulay <fboulay@nextperformance.com>
2014-06-09 16:13:49 +0200

    Better deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,6 @@
 # Used to deploy a new version of the website to the docker container
 
 git pull origin master
+DOCKER_CONTAINER=$(docker ps | grep website_docpad | awk '{print $1}')
+docker stop $DOCKER_CONTAINER
+./run.sh
--- run.sh
+++ run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash -x
 # used to  execute the website in a docker container. The exposed port 42042
 
-#docker run -p 42042:9778 -w www -v `pwd`:/www:ro fboulay/website_docpad
-CUR_DIR=`pwd`
-
-docker run -d -p 127.0.0.1:42042:9778 -v ${CUR_DIR}:/www fboulay/website_docpad
+docker run -d -p 127.0.0.1:42042:9778 -v $(pwd):/www fboulay/website_docpad
