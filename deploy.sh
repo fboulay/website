afd1ff7fb4e64135d9a4d9abb4229caee453d8d5
Florian Boulay <florian.boulay@gmail.com>
2016-09-16 09:25:24 +0200

    Add license field in package.json

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -x
 # Used to deploy a new version of the website to the docker container
 
-git fetch origin master
-git reset --hard origin/master
-DOCKER_CONTAINER=$(docker ps | grep website_docpad | awk '{print $1}')
+DOCKER_CONTAINER=$(docker ps | grep website_docpad | awk '{print $1}')``
 docker stop $DOCKER_CONTAINER
 ./build.sh
 ./run.sh
