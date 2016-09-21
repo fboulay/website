c8943509753628b2e60fe519d7c9472c98a73c7e
Florian Boulay <florian.boulay@gmail.com>
2016-09-21 11:39:59 +0200

    Fix run.sh script : renaming old running container

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # Running the current Docpad webiste in Docker
 #
 # VERSION     1.0
-# Tested with Docker 1.6
+# Tested with Docker 1.12
 
 FROM fboulay/website_docpad:1.4
 MAINTAINER Florian Boulay <florian.boulay@gmail.com>
--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 # used to  execute the website in a docker container. The exposed port 9778
 set -e
 
+docker rename running_blog $(strings /dev/urandom | grep -o '[[:alnum:]]' | head -n 30 | tr -d '\n'; echo)
 docker run -d -p 9778:9778 --name running_blog blog
